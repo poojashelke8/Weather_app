b5eaddad804d073dd9f5402b95a213801d10777f
poojashelke8 <poojabshelke0807@gmail.com>
2023-12-08 15:33:35 +0530

    Completed the project will do styling later

--- weather_app/src/Components/Weather.jsx
+++ weather_app/src/Components/Weather.jsx
@@ -1,76 +1,50 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Weather.css'
 import axios from "axios"
 
 const Weather = () => {
-  const [city,setCity] = useState('')
-  const [weather,setWeather] = useState(null)
+  const [city, setCity] = useState('')
+  const [weather, setWeather] = useState({})
 
   const apiKey = 'f397b1fd5622ae9e2cc1a3adabcaff84'
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-  const testUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f397b1fd5622ae9e2cc1a3adabcaff84`
 
-  const fetchData=async()=>{
-    try{
-      const response = await axios.get(testUrl);
-      setWeather(response.data)
-      console.log(response.data)
-    } catch (error) {
-      console.error(error);
-    }
+  const fetchData = (city) => {
+    if (!city) return
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
+    axios.get(apiURL).then((res) => {
+      setWeather(res.data)
+    }).catch((err) => {
+      console.log("err", err)
+    })
   }
 
-  // useEffect(() => {
-  //   fetchData();
-  // }, [])
-
-  let timeoutId
-
-  const debounceFetchData = () => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(fetchData, 1000); // Adjust the delay as needed
-  };
-
-  useEffect(() => {
-    debounceFetchData();
-  }, [city]); // Run whenever 'city' changes
-
-  const handleOnChange= (e)=>{
+  const handleOnChange = (e) => {
     setCity(e.target.value);
-    console.log(e.target.value);
   }
 
-  const handleSubmit = (e)=>{
-    e.preventDefault();
-    debounceFetchData()
+  const handleSubmit = () => {
+    fetchData(city)
   }
 
 
   return (
     <div className='container'>
-        <form onSubmit={handleSubmit}>
-          <input
-           type='text'
-           placeholder='Enter City'
-           value={city}
-           onChange={handleOnChange}
-           />
-           <button type='submit'>Enter</button>
-        </form>
-        {weather ? (
-        <>
-          <h2>{weather.name}</h2>
-          <p>Temperature: {weather.main.temp}°C</p>
-          <p>Description: {weather.weather[0].description}</p>
-          <p>Feels like : {weather.main.feels_like}°C</p>
-          <p>Humidity : {weather.main.humidity}%</p>
-          <p>Pressure : {weather.main.pressure}</p>
-          <p>Wind Speed : {weather.wind.speed}m/s</p>
-        </>
-      ) : (
-        <p>Loading weather data...</p>
-      )}
+      <input
+        type='text'
+        placeholder='Enter City'
+        value={city}
+        onChange={handleOnChange}
+      />
+      <button type='submit' onClick={handleSubmit}>Enter</button>
 
+      {Object.keys(weather).length > 0 &&
+        <div>
+          <h5 className="weathorCity">
+            {weather?.name}
+          </h5>
+          <h6 className="weathorTemp">{((weather?.main?.temp) - 273.15).toFixed(2)}°C</h6>
+        </div>
+      }
     </div>
   )
 }
